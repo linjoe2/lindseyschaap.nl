7d6b279907b32b095d178b6c85162ff05ca214e9
linjoe <ik@lindseyschaap.nl>
2023-06-22 13:42:01 +0200

    docs added

---
+++ docs/_app/immutable/chunks/1.2fce96a6.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.5964f91c.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2.500c77c9.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.e19e1115.js";export{t as component};
